e1ae32b5755b47b46cb41c42b42f201c632cacb9
hana03588-lab <hana03588@gmail.com>
2026-01-02 17:13:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Database</title>
+</head>
+<body>
+    <h1>Message From</h1>
+    <form action="create.php" method="POST">
+        <label>Nama :</label>
+        <input type="text" name="nama"> <br>
+
+        <label>Email :</label>
+        <input type="email" name="email"> <br>
+
+        <label>Umur :</label>
+        <input type="number" name="umur"> <br>
+        
+        <button type="submit">Simpan</button>
+    </form>
+</body>
+</html>
